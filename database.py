ddf3702d9401c85ed36c0ee83c4672210e6d704c
debasmita44 <86559877+debasmita44@users.noreply.github.com>
2025-12-09 12:25:16 +0530

    Updating code for Renderer to create database
    Updated database connection to use environment variable for DATABASE_URL.

--- database.py
+++ database.py
@@ -1,14 +1,10 @@
 # database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# SQLite database file
-DATABASE_URL = "sqlite:///./data.db"
+DATABASE_URL = os.environ.get("DATABASE_URL")  # Render sets this automatically
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
